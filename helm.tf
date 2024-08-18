5508eac07047ba20de6851ddabb555cae244d839
pman <pman-$-432894328923@gmail.com>
2024-08-18 19:15:32 +0200

    chore: update file(s): helm.tf

--- helm.tf
+++ helm.tf
@@ -1,3 +1,13 @@
+provider "helm" {
+  kubernetes {
+    host = var.k8s_host
+
+    client_certificate     = base64decode(var.k8s_client_certificate)
+    client_key             = base64decode(var.k8s_client_key)
+    cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
+  }
+}
+
 resource "helm_release" "example" {
   name       = "podinfo"
   repository = "https://stefanprodan.github.io/podinfo"
